b8d344a65a7a2b0ff622221203a63b3d9de8d690
kodera2t <kodera2t@yahoo.co.jp>
2017-06-18 15:47:52 +0900

    old esp-idf for ESP32-ADB support
    old esp-idf for ESP32-ADB and ESP32-webradio support. Please back up
    original files and swap these files with files/folders in your esp-idf
    folder

---
+++ examples/storage/nvs_rw_blob/build/mbedtls/library/ecjpake.d
@@ -0,0 +1,17 @@
+library/ecjpake.o: \
+ /Users/kodera2t/esp/esp-idf/components/mbedtls/library/ecjpake.c \
+ /Users/kodera2t/esp/esp-idf/components/mbedtls/port/include/mbedtls/esp_config.h \
+ /Users/kodera2t/esp/esp-idf/examples/storage/nvs_rw_blob/build/include/sdkconfig.h \
+ /Users/kodera2t/esp/esp-idf/components/mbedtls/include/mbedtls/check_config.h \
+ /Users/kodera2t/esp/esp-idf/components/newlib/include/limits.h \
+ /Users/kodera2t/esp/esp-idf/components/newlib/include/newlib.h
+
+/Users/kodera2t/esp/esp-idf/components/mbedtls/port/include/mbedtls/esp_config.h:
+
+/Users/kodera2t/esp/esp-idf/examples/storage/nvs_rw_blob/build/include/sdkconfig.h:
+
+/Users/kodera2t/esp/esp-idf/components/mbedtls/include/mbedtls/check_config.h:
+
+/Users/kodera2t/esp/esp-idf/components/newlib/include/limits.h:
+
+/Users/kodera2t/esp/esp-idf/components/newlib/include/newlib.h:
